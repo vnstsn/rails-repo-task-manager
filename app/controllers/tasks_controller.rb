1d1432fe8ef3b08ef56f7db5599cc5d075955d90
vanessa tsang <va.tsang@yahoo.fr>
2021-06-08 16:07:05 +0200

    wip, def index, show, new, create / edit en cours

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
