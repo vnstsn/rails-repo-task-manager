4f1ab989fbd6331d7fc937e6a5069709c981f7f0
vanessa tsang <va.tsang@yahoo.fr>
2021-06-08 16:46:06 +0200

    wip task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,15 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/tasks/:id', to: 'tasks#show', as: :task
   get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id/destroy', to: 'tasks#destroy', as: :destroy_task
 end
